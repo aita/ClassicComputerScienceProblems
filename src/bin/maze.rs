47c913558b5b8c701b94257f94ade8060e0ded35
Ryo Aita <792803+aita@users.noreply.github.com>
2023-01-02 19:59:45 +0900

    Add maze

---
+++ src/bin/maze.rs
@@ -0,0 +1,243 @@
+use bumpalo::Bump;
+use classic_computer_science_problems::generic_search::{astar, bfs, dfs, node_to_path, Graph};
+use rand::{thread_rng, Rng};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Cell {
+    Empty,
+    Blocked,
+    Start,
+    Goal,
+    Path,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct MazeLocation {
+    row: usize,
+    column: usize,
+}
+
+#[derive(Debug, Clone)]
+struct Maze {
+    rows: usize,
+    columns: usize,
+    start: MazeLocation,
+    goal: MazeLocation,
+    grid: Vec<Vec<Cell>>,
+}
+
+impl Maze {
+    fn show(&self) {
+        for row in 0..self.rows {
+            for column in 0..self.columns {
+                match self.grid[row][column] {
+                    Cell::Empty => print!("  "),
+                    Cell::Blocked => print!("🧱"),
+                    Cell::Start => print!("🚦"),
+                    Cell::Goal => print!("🏁"),
+                    Cell::Path => print!("🚶"),
+                }
+            }
+            println!();
+        }
+    }
+
+    fn goal_test(&self, loc: &MazeLocation) -> bool {
+        loc == &self.goal
+    }
+
+    fn successors(&self, loc: &MazeLocation) -> Vec<MazeLocation> {
+        let mut locations = Vec::new();
+        if loc.row + 1 < self.rows && self.grid[loc.row + 1][loc.column] != Cell::Blocked {
+            locations.push(MazeLocation {
+                row: loc.row + 1,
+                column: loc.column,
+            });
+        }
+        if loc.row > 0 && self.grid[loc.row - 1][loc.column] != Cell::Blocked {
+            locations.push(MazeLocation {
+                row: loc.row - 1,
+                column: loc.column,
+            });
+        }
+        if loc.column + 1 < self.columns && self.grid[loc.row][loc.column + 1] != Cell::Blocked {
+            locations.push(MazeLocation {
+                row: loc.row,
+                column: loc.column + 1,
+            });
+        }
+        if loc.column > 0 && self.grid[loc.row][loc.column - 1] != Cell::Blocked {
+            locations.push(MazeLocation {
+                row: loc.row,
+                column: loc.column - 1,
+            });
+        }
+        locations
+    }
+
+    fn marked(&self, path: Vec<MazeLocation>) -> Self {
+        let mut maze = self.clone();
+        for loc in path {
+            maze.grid[loc.row][loc.column] = Cell::Path;
+        }
+        maze.grid[maze.start.row][maze.start.column] = Cell::Start;
+        maze.grid[maze.goal.row][maze.goal.column] = Cell::Goal;
+        maze
+    }
+}
+
+#[derive(Default)]
+struct MazeBuilder {
+    rows: Option<usize>,
+    columns: Option<usize>,
+    sparseness: Option<f64>,
+    start: Option<MazeLocation>,
+    goal: Option<MazeLocation>,
+}
+
+impl MazeBuilder {
+    fn rows(mut self, rows: usize) -> Self {
+        self.rows = Some(rows);
+        self
+    }
+
+    fn columns(mut self, columns: usize) -> Self {
+        self.columns = Some(columns);
+        self
+    }
+
+    fn sparseness(mut self, sparseness: f64) -> Self {
+        self.sparseness = Some(sparseness);
+        self
+    }
+
+    fn start(mut self, start: MazeLocation) -> Self {
+        self.start = Some(start);
+        self
+    }
+
+    fn goal(mut self, goal: MazeLocation) -> Self {
+        self.goal = Some(goal);
+        self
+    }
+
+    fn build(self) -> Maze {
+        let rows = self.rows.unwrap_or(10);
+        let columns = self.columns.unwrap_or(10);
+        let sparseness = self.sparseness.unwrap_or(0.2);
+        let start = self.start.unwrap_or(MazeLocation { row: 0, column: 0 });
+        let goal = self.goal.unwrap_or(MazeLocation {
+            row: rows - 1,
+            column: columns - 1,
+        });
+
+        let mut grid = vec![vec![Cell::Empty; columns]; rows];
+
+        // fill in the blocked cells with a random sparseness
+        let mut rng = thread_rng();
+        for row in 0..rows {
+            for column in 0..columns {
+                if rng.gen::<f64>() < sparseness {
+                    grid[row][column] = Cell::Blocked;
+                }
+            }
+        }
+
+        grid[start.row][start.column] = Cell::Start;
+        grid[goal.row][goal.column] = Cell::Goal;
+
+        Maze {
+            rows,
+            columns,
+            start,
+            goal,
+            grid,
+        }
+    }
+}
+
+fn euclidean_distance(goal: &MazeLocation) -> impl Fn(&MazeLocation) -> f64 + '_ {
+    |loc| {
+        let xdist = (loc.column as f64) - (goal.column as f64);
+        let ydist = (loc.row as f64) - (goal.row as f64);
+        (xdist * xdist + ydist * ydist).sqrt()
+    }
+}
+
+fn manhattan_distance(goal: &MazeLocation) -> impl Fn(&MazeLocation) -> f64 + '_ {
+    |loc| {
+        let xdist = (loc.column as f64) - (goal.column as f64);
+        let ydist = (loc.row as f64) - (goal.row as f64);
+        xdist.abs() + ydist.abs()
+    }
+}
+
+fn main() {
+    let maze = MazeBuilder::default()
+        .rows(10)
+        .columns(10)
+        .sparseness(0.2)
+        .start(MazeLocation { row: 0, column: 0 })
+        .goal(MazeLocation { row: 9, column: 9 })
+        .build();
+
+    println!("Maze:");
+    maze.show();
+
+    let bump = Bump::new();
+
+    {
+        println!("\nSolving maze using depth-first search:");
+        let graph = Graph::new(&bump);
+        let solution = dfs(
+            &graph,
+            &maze.start,
+            |loc| maze.goal_test(loc),
+            |loc| maze.successors(loc),
+        );
+        if let Some(node) = solution {
+            let path = node_to_path(node);
+            let maze = maze.marked(path);
+            maze.show();
+        } else {
+            println!("No solution found using depth-first search!");
+        }
+    }
+
+    {
+        println!("\nSolving maze using breadth-first search:");
+        let graph = Graph::new(&bump);
+        let solution = bfs(
+            &graph,
+            &maze.start,
+            |loc| maze.goal_test(loc),
+            |loc| maze.successors(loc),
+        );
+        if let Some(node) = solution {
+            let path = node_to_path(node);
+            let maze = maze.marked(path);
+            maze.show();
+        } else {
+            println!("No solution found using breadth-first search!");
+        }
+    }
+
+    {
+        println!("\nSolving maze using A* search:");
+        let graph = Graph::new(&bump);
+        let solution = astar(
+            &graph,
+            &maze.start,
+            |loc| maze.goal_test(loc),
+            |loc| maze.successors(loc),
+            manhattan_distance(&maze.goal),
+        );
+        if let Some(node) = solution {
+            let path = node_to_path(node);
+            let maze = maze.marked(path);
+            maze.show();
+        } else {
+            println!("No solution found using A* search!");
+        }
+    }
+}
---
+++ src/generic_search.rs
@@ -0,0 +1,197 @@
+use approx::relative_eq;
+use bumpalo::Bump;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::hash::Hash;
+use std::marker::PhantomData;
+
+pub struct Graph<'bump, T> {
+    bump: &'bump Bump,
+    phantom: PhantomData<T>,
+}
+
+impl<'bump, T> Graph<'bump, T> {
+    pub fn new(bump: &'bump Bump) -> Self {
+        Self {
+            bump,
+            phantom: PhantomData,
+        }
+    }
+
+    pub fn add_node(&self, node: Node<'bump, T>) -> &'bump Node<T> {
+        let node = self.bump.alloc(node);
+        node
+    }
+}
+
+#[derive(Debug)]
+pub struct Node<'bump, T> {
+    pub state: T,
+    pub parent: Option<&'bump Node<'bump, T>>,
+    pub cost: f64,
+    pub heuristic: f64,
+}
+
+impl<'bump, T> Node<'bump, T> {
+    pub fn new(state: T, parent: Option<&'bump Node<'bump, T>>) -> Self {
+        Self {
+            state,
+            parent: parent,
+            cost: 0.0,
+            heuristic: 0.0,
+        }
+    }
+
+    pub fn with_heuristic(
+        state: T,
+        parent: Option<&'bump Node<'bump, T>>,
+        cost: f64,
+        heuristic: f64,
+    ) -> Self {
+        Self {
+            state,
+            parent: parent,
+            cost,
+            heuristic,
+        }
+    }
+}
+
+impl<'bump, T> PartialEq for Node<'bump, T> {
+    fn eq(&self, other: &Self) -> bool {
+        relative_eq!(self.cost + self.heuristic, other.cost + other.heuristic)
+    }
+}
+
+impl<'bump, T> Eq for Node<'bump, T> {}
+
+impl<'bump, T> Ord for Node<'bump, T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let self_priority = self.cost + self.heuristic;
+        let other_priority = other.cost + other.heuristic;
+        other_priority.partial_cmp(&self_priority).unwrap()
+    }
+}
+
+impl<'bump, T> PartialOrd for Node<'bump, T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+pub fn dfs<'bump, T, G, S>(
+    graph: &'bump Graph<'bump, T>,
+    initial: &T,
+    goal_test: G,
+    successors: S,
+) -> Option<&'bump Node<'bump, T>>
+where
+    T: Eq + Hash + Clone,
+    G: Fn(&T) -> bool,
+    S: Fn(&T) -> Vec<T>,
+{
+    let mut frontier = Vec::new();
+    frontier.push(graph.add_node(Node::new(initial.clone(), None)));
+
+    let mut explored = HashSet::new();
+    explored.insert(initial.clone());
+
+    while let Some(node) = frontier.pop() {
+        if goal_test(&node.state) {
+            return Some(node);
+        }
+        for child in successors(&node.state) {
+            if !explored.contains(&child) {
+                explored.insert(child.clone());
+                frontier.push(graph.add_node(Node::new(child.clone(), Some(node))));
+            }
+        }
+    }
+
+    None
+}
+
+pub fn node_to_path<T: Clone>(node: &Node<T>) -> Vec<T> {
+    let mut path = vec![node.state.clone()];
+    let mut current = node;
+    while let Some(parent) = current.parent {
+        current = parent;
+        path.push(current.state.clone());
+    }
+    path.reverse();
+    path
+}
+
+pub fn bfs<'bump, T, G, S>(
+    graph: &'bump Graph<'bump, T>,
+    initial: &T,
+    goal_test: G,
+    successors: S,
+) -> Option<&'bump Node<'bump, T>>
+where
+    T: Eq + Hash + Clone,
+    G: Fn(&T) -> bool,
+    S: Fn(&T) -> Vec<T>,
+{
+    let mut frontier = VecDeque::new();
+    frontier.push_back(graph.add_node(Node::new(initial.clone(), None)));
+
+    let mut explored = HashSet::new();
+    explored.insert(initial.clone());
+
+    while let Some(node) = frontier.pop_front() {
+        if goal_test(&node.state) {
+            return Some(node);
+        }
+        for child in successors(&node.state) {
+            if !explored.contains(&child) {
+                explored.insert(child.clone());
+                frontier.push_back(graph.add_node(Node::new(child.clone(), Some(node))));
+            }
+        }
+    }
+
+    None
+}
+
+pub fn astar<'bump, T, G, S, H>(
+    graph: &'bump Graph<'bump, T>,
+    initial: &T,
+    goal_test: G,
+    successors: S,
+    heuristic: H,
+) -> Option<&'bump Node<'bump, T>>
+where
+    T: Eq + Hash + Clone,
+    G: Fn(&T) -> bool,
+    S: Fn(&T) -> Vec<T>,
+    H: Fn(&T) -> f64,
+{
+    let mut frontier = BinaryHeap::new();
+    frontier.push(graph.add_node(Node::with_heuristic(
+        initial.clone(),
+        None,
+        0.0,
+        heuristic(initial),
+    )));
+
+    let mut explored = HashMap::new();
+    explored.insert(initial.clone(), 0.0);
+
+    while let Some(node) = frontier.pop() {
+        if goal_test(&node.state) {
+            return Some(node);
+        }
+        for child in successors(&node.state) {
+            let new_cost = node.cost + 1.0;
+            if explored.get(&child).is_none() || explored[&child] > new_cost {
+                explored.insert(child.clone(), new_cost);
+                let node =
+                    Node::with_heuristic(child.clone(), Some(node), new_cost, heuristic(&child));
+                frontier.push(graph.add_node(node));
+            }
+        }
+    }
+
+    None
+}
