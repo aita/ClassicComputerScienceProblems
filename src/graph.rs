389f50dcf12e292f65a7d9ac8d1afd551895e9a3
Ryo Aita <792803+aita@users.noreply.github.com>
2023-01-08 04:19:53 +0900

    Add a weighted graph

--- src/graph.rs
+++ src/graph.rs
@@ -9,6 +9,14 @@ impl Edge {
     pub fn reversed(&self) -> Self {
         Edge(self.1, self.0)
     }
+
+    pub fn u(&self) -> usize {
+        self.0
+    }
+
+    pub fn v(&self) -> usize {
+        self.1
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -87,6 +95,123 @@ where
     }
 
     pub fn edges_for_vertex(&self, vertex: &Vertex) -> Vec<&Edge> {
+        self.edges_for_index(self.index_of(vertex))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct WeightedEdge(usize, usize, f64);
+
+impl WeightedEdge {
+    pub fn new(a: usize, b: usize, weight: f64) -> Self {
+        WeightedEdge(a, b, weight)
+    }
+
+    pub fn reversed(&self) -> Self {
+        WeightedEdge(self.1, self.0, self.2)
+    }
+
+    pub fn u(&self) -> usize {
+        self.0
+    }
+
+    pub fn v(&self) -> usize {
+        self.1
+    }
+
+    pub fn weight(&self) -> f64 {
+        self.2
+    }
+}
+
+impl PartialOrd for WeightedEdge {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.2.partial_cmp(&other.2)
+    }
+}
+
+pub struct WeightedGraph<Vertex> {
+    vertices: Vec<Vertex>,
+    edges: Vec<Vec<WeightedEdge>>,
+}
+
+impl<Vertex> WeightedGraph<Vertex>
+where
+    Vertex: PartialEq + Clone,
+{
+    pub fn new(vertices: Vec<Vertex>) -> Self {
+        let edges = vec![Vec::new(); vertices.len()];
+        Self { vertices, edges }
+    }
+
+    pub fn vertices(&self) -> &Vec<Vertex> {
+        &self.vertices
+    }
+
+    pub fn edges(&self) -> &Vec<Vec<WeightedEdge>> {
+        &self.edges
+    }
+
+    pub fn vertex_count(&self) -> usize {
+        self.vertices.len()
+    }
+
+    pub fn edge_count(&self) -> usize {
+        self.edges.iter().map(|e| e.len()).sum()
+    }
+
+    pub fn add_vertex(&mut self, vertex: Vertex) -> usize {
+        self.vertices.push(vertex);
+        self.edges.push(Vec::new());
+        self.vertices.len() - 1
+    }
+
+    pub fn add_edge(&mut self, edge: WeightedEdge) {
+        self.edges[edge.0].push(edge.clone());
+        self.edges[edge.1].push(edge.reversed());
+    }
+
+    pub fn add_edge_by_indices(&mut self, u: usize, v: usize, weight: f64) {
+        self.add_edge(WeightedEdge::new(u, v, weight));
+    }
+
+    pub fn add_edge_by_vertices(&mut self, first: &Vertex, second: &Vertex, weight: f64) {
+        let u = self.vertices.iter().position(|v| v == first).unwrap();
+        let v = self.vertices.iter().position(|v| v == second).unwrap();
+        self.add_edge_by_indices(u, v, weight);
+    }
+
+    pub fn vertex_at(&self, index: usize) -> &Vertex {
+        &self.vertices[index]
+    }
+
+    pub fn index_of(&self, vertex: &Vertex) -> usize {
+        self.vertices.iter().position(|v| v == vertex).unwrap()
+    }
+
+    pub fn neighbors_for_index(&self, index: usize) -> Vec<Vertex> {
+        self.edges[index]
+            .iter()
+            .map(|e| self.vertex_at(e.1).clone())
+            .collect()
+    }
+
+    pub fn neighbors_for_index_with_weights(&self, index: usize) -> Vec<(Vertex, f64)> {
+        self.edges[index]
+            .iter()
+            .map(|e| (self.vertex_at(e.1).clone(), e.2))
+            .collect()
+    }
+
+    pub fn neighbors_for_vertex(&self, vertex: &Vertex) -> Vec<Vertex> {
+        self.neighbors_for_index(self.index_of(vertex))
+    }
+
+    pub fn edges_for_index(&self, index: usize) -> Vec<&WeightedEdge> {
+        self.edges[index].iter().collect()
+    }
+
+    pub fn edges_for_vertex(&self, vertex: &Vertex) -> Vec<&WeightedEdge> {
         self.edges_for_index(self.index_of(vertex))
     }
 }
@@ -170,5 +295,61 @@ mod tests {
             node_to_path(bfs_result.unwrap()),
             vec!["Boston", "Detroit", "Washington", "Miami"]
         )
+    }
+
+    #[test]
+    fn test_weighted_graph() {
+        let mut city_graph = WeightedGraph::new(vec![
+            "Seattle",
+            "San Francisco",
+            "Los Angeles",
+            "Riverside",
+            "Phoenix",
+            "Chicago",
+            "Boston",
+            "New York",
+            "Atlanta",
+            "Miami",
+            "Dallas",
+            "Houston",
+            "Detroit",
+            "Philadelphia",
+            "Washington",
+        ]);
+
+        city_graph.add_edge_by_vertices(&"Seattle", &"Chicago", 1737.0);
+        city_graph.add_edge_by_vertices(&"Seattle", &"San Francisco", 678.0);
+        city_graph.add_edge_by_vertices(&"San Francisco", &"Riverside", 386.0);
+        city_graph.add_edge_by_vertices(&"San Francisco", &"Los Angeles", 348.0);
+        city_graph.add_edge_by_vertices(&"Los Angeles", &"Riverside", 50.0);
+        city_graph.add_edge_by_vertices(&"Los Angeles", &"Phoenix", 357.0);
+        city_graph.add_edge_by_vertices(&"Riverside", &"Phoenix", 307.0);
+        city_graph.add_edge_by_vertices(&"Riverside", &"Chicago", 1704.0);
+        city_graph.add_edge_by_vertices(&"Phoenix", &"Dallas", 887.0);
+        city_graph.add_edge_by_vertices(&"Phoenix", &"Houston", 1015.0);
+        city_graph.add_edge_by_vertices(&"Dallas", &"Chicago", 805.0);
+        city_graph.add_edge_by_vertices(&"Dallas", &"Atlanta", 721.0);
+        city_graph.add_edge_by_vertices(&"Dallas", &"Houston", 225.0);
+        city_graph.add_edge_by_vertices(&"Houston", &"Atlanta", 702.0);
+        city_graph.add_edge_by_vertices(&"Houston", &"Miami", 968.0);
+        city_graph.add_edge_by_vertices(&"Atlanta", &"Chicago", 588.0);
+        city_graph.add_edge_by_vertices(&"Atlanta", &"Washington", 543.0);
+        city_graph.add_edge_by_vertices(&"Atlanta", &"Miami", 604.0);
+        city_graph.add_edge_by_vertices(&"Miami", &"Washington", 923.0);
+        city_graph.add_edge_by_vertices(&"Chicago", &"Detroit", 238.0);
+        city_graph.add_edge_by_vertices(&"Detroit", &"Boston", 613.0);
+        city_graph.add_edge_by_vertices(&"Detroit", &"Washington", 396.0);
+        city_graph.add_edge_by_vertices(&"Detroit", &"New York", 482.0);
+        city_graph.add_edge_by_vertices(&"Boston", &"New York", 190.0);
+        city_graph.add_edge_by_vertices(&"New York", &"Philadelphia", 81.0);
+        city_graph.add_edge_by_vertices(&"Philadelphia", &"Washington", 123.0);
+
+        for i in 0..city_graph.vertex_count() {
+            println!(
+                "{} -> {:?}",
+                city_graph.vertex_at(i),
+                city_graph.neighbors_for_index_with_weights(i)
+            );
+        }
     }
 }
